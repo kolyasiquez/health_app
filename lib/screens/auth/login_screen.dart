082dad49d86738899de20ed4bb9f5103ed2af267
kolyasiquez <133633724+kolyasiquez@users.noreply.github.com>
2025-09-22 11:29:16 +0200

    fixed small bug in interface

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -14,53 +14,55 @@ class LoginScreen extends StatelessWidget {
             end: Alignment.bottomCenter,
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(Icons.favorite, size: 100, color: Colors.deepPurpleAccent),
-              const SizedBox(height: 20),
-              Text(
-                'Мобільний медичний помічник',
-                style: Theme.of(context).textTheme.headlineSmall,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 40),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Електронна пошта',
-                  prefixIcon: Icon(Icons.email, color: Colors.deepPurpleAccent),
+        child: SingleChildScrollView( // <-- Додали SingleChildScrollView тут
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.favorite, size: 100, color: Colors.deepPurpleAccent),
+                const SizedBox(height: 20),
+                Text(
+                  'Мобільний медичний помічник',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              const SizedBox(height: 20),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Пароль',
-                  prefixIcon: Icon(Icons.lock, color: Colors.deepPurpleAccent),
+                const SizedBox(height: 40),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Електронна пошта',
+                    prefixIcon: Icon(Icons.email, color: Colors.deepPurpleAccent),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  // Логіка входу
-                  Navigator.pushReplacementNamed(context, '/patient_dashboard');
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(60),
+                const SizedBox(height: 20),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Пароль',
+                    prefixIcon: Icon(Icons.lock, color: Colors.deepPurpleAccent),
+                  ),
                 ),
-                child: const Text('Увійти'),
-              ),
-              const SizedBox(height: 10),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Зареєструватися',
-                  style: TextStyle(color: Colors.deepPurpleAccent.shade200),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    // Логіка входу
+                    Navigator.pushReplacementNamed(context, '/patient_dashboard');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(60),
+                  ),
+                  child: const Text('Увійти'),
                 ),
-              ),
-            ],
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Зареєструватися',
+                    style: TextStyle(color: Colors.deepPurpleAccent.shade200),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
